9226582c9dff3b248f3dddb3868ad70f45bea3af
jan30chen <jan30chen@qq.com>
2020-05-18 23:01:46 +0800

    插件引入，home页面编写

---
+++ pages/home/home.js
@@ -0,0 +1,87 @@
+// pages/home/home.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        active: 2,
+        account: [
+          {id:1, type:"衣", money:-100, detail:"上衣"},
+          {id:2, type:"衣", money:-100, detail:"上衣"},
+          {id:3, type:"衣", money:-100, detail:"上衣"},
+          {id:4, type:"衣", money:-100, detail:"上衣"},
+          {id:5, type:"衣", money:-100, detail:"上衣"},
+          {id:6, type:"衣", money:-100, detail:"上衣"}
+        ]
+    },
+
+    onClick(event){
+      wx.showToast({
+        title: `点击了按钮`,
+      })
+    },
+
+    onChange(event) {
+        wx.showToast({
+          title: `切换到标签 ${event.detail.name+1}`,
+          icon: 'none',
+        });
+      },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
+})
