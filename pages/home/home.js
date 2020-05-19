a1ed0d279ff82284f379573a9cda88cf5ad14d4c
jan30chen <jan30chen@qq.com>
2020-05-19 22:47:35 +0800

    完善home页面，后台添加部分功能

--- pages/home/home.js
+++ pages/home/home.js
@@ -7,19 +7,33 @@ Page({
     data: {
         active: 2,
         account: [
-          {id:1, type:"衣", money:-100, detail:"上衣"},
-          {id:2, type:"衣", money:-100, detail:"上衣"},
-          {id:3, type:"衣", money:-100, detail:"上衣"},
-          {id:4, type:"衣", money:-100, detail:"上衣"},
-          {id:5, type:"衣", money:-100, detail:"上衣"},
-          {id:6, type:"衣", money:-100, detail:"上衣"}
-        ]
+          {id:1, type:0, money:-100, detail:"1上衣"},
+          {id:2, type:1, money:-100, detail:"2上衣"},
+          {id:3, type:2, money:-100, detail:"3上衣"},
+          {id:4, type:3, money:-100, detail:"4上衣"},
+          {id:5, type:3, money:-100, detail:"5上衣"},
+          {id:6, type:3, money:-100, detail:"6上衣"},
+        ],
+        income: 0,
+        outcome: 200,
+        money_type:["衣","食","住","行"],
+        money_type_color:["#1989fa","#07c160","#ee0a24","#ff976a"],
+        count_money:"",
     },
 
     onClick(event){
       wx.showToast({
         title: `点击了按钮`,
       })
+    },
+
+    countMoney(){
+      let money = 0;
+      let account = this.data.account;
+      for (let index in account) {
+        money += account[index].money;
+       }
+      return money;
     },
 
     onChange(event) {
@@ -40,7 +54,9 @@ Page({
      * 生命周期函数--监听页面初次渲染完成
      */
     onReady: function () {
-
+      this.setData({
+        count_money: this.countMoney()
+      })
     },
 
     /**
