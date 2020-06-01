ea0dbdd8df916a59569aa0f008a73d3c9e242f93
jan30chen <jan30chen@qq.com>
2020-06-01 18:12:24 +0800

    Info页面修复：添加数据获取

--- pages/info/info.js
+++ pages/info/info.js
@@ -7,15 +7,6 @@ Page({
     data: {
         item_id: undefined,
         item: { id:1, type:"暂缺", money:"暂缺", detail:"暂缺", date:"暂缺" },
-        account: [
-            { id:1, type:4, money:100, detail:"打工", date:"2020.1.4" },
-            { id:2, type:1, money:-23, detail:"吃饭", date:"2020.1.4"  },
-            { id:4, type:3, money:-300, detail:"玩", date:"2020.1.4"  },
-            { id:5, type:3, money:-500, detail:"买衣服", date:"2020.1.4"  },
-            { id:6, type:6, money:1000, detail:"打工", date:"2020.1.4"  },
-            { id:6, type:1, money:-230, detail:"聚餐", date:"2020.1.4"  },
-        ],
-
     },
 
     /**
@@ -23,13 +14,32 @@ Page({
      */
     onLoad: function (options) {
         this.setData({ item_id: options.item_id });
-        let _account = this.data.account;
+        this.getData();
+    },
+
+    getData(){
+        const that = this;
+        wx.request({
+          url: 'https://example.com/ajax/account',
+          dataType: 'json',
+          success(res) {
+            that.setData({  account: res.data.account });
+            that.getItem();
+          },  fail(err){
+            console.error(err.errMsg);
+          }
+        })
+      },
+
+    getItem(){
+      let _account = this.data.account;
         
-        for( let i=0; i<_account.length; i++){
-            if( _account[i].id==options.item_id ){
-                this.setData({ item: _account[i] })
-            }
-        }
+      for( let i=0; i<_account.length; i++){
+          if( _account[i].id==this.data.item_id ){
+              this.setData({ item: _account[i] })
+              return
+          }
+      }
     },
 
     onDel(){
